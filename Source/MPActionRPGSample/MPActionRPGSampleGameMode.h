fcdad9812bf928971901fa3de988dfcdc0b946a9
smilewook <56058684+smilewook@users.noreply.github.com>
2026-07-09 17:04:00 +0900

    [Init] Create UE5 multiplayer action RPG sample project

---
+++ Source/MPActionRPGSample/MPActionRPGSampleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MPActionRPGSampleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMPActionRPGSampleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMPActionRPGSampleGameMode();
+};
+
+
+
